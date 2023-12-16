404f84d4eea614652c39fd1527343212e53d057a
klimisnotavailable <klimgrigor28@gmail.com>
2023-12-16 22:42:11 +0200

    дз

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        return message
+    }
+    else if (message.length > maxLength) {
+       const slicedMessage =  message.slice(0,maxLength)
+        return `${slicedMessage}...`
+    }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16))
+console.log(formatMessage("Curabitur ligula sapien", 23))
+console.log(formatMessage("Vestibulum facilisis purus nec", 20))
+console.log(formatMessage("Vestibulum facilisis purus nec", 30))
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15))
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41))
